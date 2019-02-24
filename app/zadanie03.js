7af633f1e75ecf15e208c97fbcd2d812e561dd94
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-02-24 19:05:21 +0100

    finished tasks: zadanie03, zadanieDnia

--- app/zadanie03.js
+++ app/zadanie03.js
@@ -1,1 +1,10 @@
 //Twój kod
+const sum = () => {
+    let mySum = 0;
+    for(let i = 2; i < process.argv.length; i++) {
+        let myArg = Number(process.argv[i]);
+        mySum += myArg;
+    };
+    return console.log( mySum );
+}
+sum();
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,12 @@
 //Twój kod
+const sleepSort = () => {
+    // let mySum = 0;
+    for(let i = 2; i < process.argv.length; i++) {
+        let myArg = Number(process.argv[i]);
+        setTimeout(() => {
+            return console.log( myArg );
+        }, myArg*1000)
+    };
+
+}
+sleepSort();
